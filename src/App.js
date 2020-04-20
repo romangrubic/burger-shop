f925fbb2bc46a6dfc1e57eb784282fc456f631cc
Roman Grubic <grubic.roman@gmail.com>
2020-04-20 13:07:14 +0000

    Added a check if no ingredients in burger.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import classes from './App.module.css';
+// import classes from './App.module.css';
 
 class App extends Component {
     render() {
