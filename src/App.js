0c8959d744a993b5c4875b7aed83986d61c3e982
Roman Grubic <grubic.roman@gmail.com>
2020-04-20 09:54:27 +0000

    Creating a Layout component and placing googlefonts.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
 import classes from './App.module.css';
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <p>Test</p>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
