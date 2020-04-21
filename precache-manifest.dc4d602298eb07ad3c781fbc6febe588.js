61674d3d91efa26253baf61aba50a5e7fcbe6ba4
Roman Grubic <grubic.roman@gmail.com>
2020-04-21 18:15:20 +0000

    Updates

---
+++ precache-manifest.dc4d602298eb07ad3c781fbc6febe588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb5c18ef05b1a24e522031fd46961ea",
+    "url": "/burger-shop/index.html"
+  },
+  {
+    "revision": "e96756647d7a1c2721cc",
+    "url": "/burger-shop/static/css/main.486a47d1.chunk.css"
+  },
+  {
+    "revision": "023c30f9b562b7d71e57",
+    "url": "/burger-shop/static/js/2.8072970c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-shop/static/js/2.8072970c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96756647d7a1c2721cc",
+    "url": "/burger-shop/static/js/main.6ea40aff.chunk.js"
+  },
+  {
+    "revision": "62a999b1adf78dab21a0",
+    "url": "/burger-shop/static/js/runtime-main.ee3674a3.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-shop/static/media/Burger.b8503d26.png"
+  }
+]);
