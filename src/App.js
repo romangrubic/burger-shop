a001a4a9fc2153b662b20c6dbec97a921c53bfe6
Roman Grubic <grubic.roman@gmail.com>
2020-04-25 13:37:01 +0000

    Added orders component, fetching data from Firebase and outputting orders.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 // import classes from './App.module.css';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Route, Switch } from 'react-router-dom';
 
 
@@ -14,6 +15,7 @@ class App extends Component {
                     {/* switch will load the first hit page and exact loads exactly that url.. choose one or both */}
                     <Switch>
                         <Route path="/checkout" component={Checkout} />
+                        <Route path="/orders" component={Orders} />
                         <Route path="/" exact component={BurgerBuilder} />
                     </Switch>
                 </Layout>
