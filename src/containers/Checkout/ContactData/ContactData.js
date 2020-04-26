2980ff856f97238469af9792834ae7a8219d724a
Roman Grubic <grubic.roman@gmail.com>
2020-04-26 10:59:09 +0000

    Handling form submission.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,9 +65,14 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault();
         this.setState({ loading: true })
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
 
         }
         axios.post('/orders.json', order)
@@ -97,7 +102,7 @@ class ContactData extends Component {
             });
         }
 
-        let form = (<form>
+        let form = (<form onSubmit={this.orderHandler}>
             { formElementsArray.map(formElement => (
                 <Input
                     key={ formElement.id }
