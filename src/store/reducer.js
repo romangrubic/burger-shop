6c4ed300256f3531d4fe0767d4906055691889d5
Roman Grubic <grubic.roman@gmail.com>
2020-04-28 09:53:25 +0000

    Setting store and initial state.

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 5,
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
