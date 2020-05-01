165dd646f95a18604cfdf519144dd193ceac97d3
Roman Grubic <grubic.roman@gmail.com>
2020-05-01 16:15:24 +0000

    Managing log out state.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,6 +21,19 @@ export const authFail = (error) => {
         error: error
     };
 };
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+export const checkAuthTimeout = (expirationTime) =>{
+    return dispatch => {
+        setTimeout(() =>{
+            dispatch(logout())
+        }, expirationTime * 1000)
+    }
+}
+
 
 export const auth = (email, password, isSignup) =>{
     return dispatch =>{
@@ -37,7 +50,8 @@ export const auth = (email, password, isSignup) =>{
         axios.post(url, authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data.idToken, response.data.localId))
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
         })
         .catch(err => {
             console.log(err);
