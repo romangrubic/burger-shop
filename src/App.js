4503fb228bc4157d26d64c3f829eb716bff9f94a
Roman Grubic <grubic.roman@gmail.com>
2020-05-02 16:21:10 +0000

    Guarding routes from unauthorized.

--- src/App.js
+++ src/App.js
@@ -4,32 +4,50 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 // import classes from './App.module.css';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 
 
 class App extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.onTryAutoSignup();
     }
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/burger-shop/auth" component={ Auth } />
+                <Route path="/burger-shop" exact component={ BurgerBuilder } />
+                <Redirect to='/burger-shop' />
+            </Switch>
+        );
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/burger-shop/checkout" component={ Checkout } />
+                    <Route path="/burger-shop/orders" component={ Orders } />
+                    <Route path="/burger-shop/logout" component={ Logout } />
+                    <Route path="/burger-shop" exact component={ BurgerBuilder } />
+                    <Redirect to='/burger-shop' />
+                </Switch>
+            );
+        }
         return (
             <div>
                 <Layout>
-                    {/* switch will load the first hit page and exact loads exactly that url.. choose one or both */}
-                    <Switch>
-                        <Route path="/burger-shop/checkout" component={Checkout} />
-                        <Route path="/burger-shop/orders" component={Orders} />
-                        <Route path="/burger-shop/auth" component={Auth} />
-                        <Route path="/burger-shop/logout" component={Logout} />
-                        <Route path="/burger-shop" exact component={BurgerBuilder} />
-                    </Switch>
+                    {/* switch will load the first hit page and exact loads exactly that url.. choose one or both */ }
+                    { routes }
                 </Layout>
             </div>
         );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
     }
 }
 
@@ -39,4 +57,4 @@ const mapDispatchToProps = dispatch => {
     };
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
