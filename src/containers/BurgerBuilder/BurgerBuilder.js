a98d165e13041209fc947fe20cc66d75d04c691d
Roman Grubic <grubic.roman@gmail.com>
2020-05-06 19:56:41 +0000

    Converting ContactData and BurgerBuilder.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from 'react-redux';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
@@ -12,16 +12,14 @@ import * as actions from '../../store/actions/index';
 
 
 // to test container we should export them and then we can test them
-export class BurgerBuilder extends Component {
-    state = {
-        purchasing: false,
-    }
+export const BurgerBuilder = props => {
+    const [purchasing, setPurchasing] = useState(false)
 
-    componentDidMount() {
-        this.props.onInitIngredients()
-    }
+    useEffect(() => {
+        props.onInitIngredients()
+    }, [])
 
-    updatePurchaseState(ingredients) {
+    const updatePurchaseState = (ingredients) => {
         const sum = Object.keys(ingredients)
             .map(igKey => {
                 return ingredients[igKey]
@@ -32,64 +30,63 @@ export class BurgerBuilder extends Component {
         return sum > 0;
     }
 
-    purchaseHandler = () => {
-        if (this.props.isAuthenticated) {
-            this.setState({ purchasing: true })
+    const purchaseHandler = () => {
+        if (props.isAuthenticated) {
+            setPurchasing(true)
         } else {
-            this.props.onSetAuthRedirectPath('/burger-shop/checkout');
-            this.props.history.push('/burger-shop/auth');
+            props.onSetAuthRedirectPath('/burger-shop/checkout');
+            props.history.push('/burger-shop/auth');
         };
     }
 
-    purchaseCancelHandler = () => {
-        this.setState({ purchasing: false });
+    const purchaseCancelHandler = () => {
+        setPurchasing(false)
     }
 
-    purchaseContinueHandler = () => {
-        this.props.onInitPurchase();
-        this.props.history.push('/burger-shop/checkout');
+    const purchaseContinueHandler = () => {
+        props.onInitPurchase();
+        props.history.push('/burger-shop/checkout');
     }
 
-    render() {
-        const disabledInfo = {
-            ...this.props.ings
-        };
-        for (let key in disabledInfo) {
-            disabledInfo[key] = disabledInfo[key] <= 0
-        }
-        let orderSummary = null;
-        let burger = this.props.error ? <p>Ingredients cant be loaded</p> : <Spinner />;
-        if (this.props.ings) {
-            burger = (
-                <Aux>
-                    <Burger ingredients={ this.props.ings } />
-                    <BuildControls
-                        ingredientAdded={ this.props.onIngredientAdded }
-                        ingredientRemoved={ this.props.onIngredientRemoved }
-                        disabled={ disabledInfo }
-                        purchasable={ this.updatePurchaseState(this.props.ings) }
-                        ordered={ this.purchaseHandler }
-                        isAuth={ this.props.isAuthenticated }
-                        price={ this.props.price } />
-                </Aux>
-            );
-            orderSummary = <OrderSummary
-                ingredients={ this.props.ings }
-                purchaseCancelled={ this.purchaseCancelHandler }
-                purchaseContinue={ this.purchaseContinueHandler }
-                price={ this.props.price } />
-        };
-        return (
+    const disabledInfo = {
+        ...props.ings
+    };
+    for (let key in disabledInfo) {
+        disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    let orderSummary = null;
+    let burger = props.error ? <p>Ingredients cant be loaded</p> : <Spinner />;
+    if (props.ings) {
+        burger = (
             <Aux>
-                <Modal
-                    show={ this.state.purchasing }
-                    modalClosed={ this.purchaseCancelHandler }>
-                    { orderSummary }
-                </Modal>
-                { burger }
+                <Burger ingredients={ props.ings } />
+                <BuildControls
+                    ingredientAdded={ props.onIngredientAdded }
+                    ingredientRemoved={ props.onIngredientRemoved }
+                    disabled={ disabledInfo }
+                    purchasable={ updatePurchaseState(props.ings) }
+                    ordered={ purchaseHandler }
+                    isAuth={ props.isAuthenticated }
+                    price={ props.price } />
             </Aux>
         );
-    }
+        orderSummary = <OrderSummary
+            ingredients={ props.ings }
+            purchaseCancelled={ purchaseCancelHandler }
+            purchaseContinue={ purchaseContinueHandler }
+            price={ props.price } />
+    };
+    return (
+        <Aux>
+            <Modal
+                show={ purchasing }
+                modalClosed={ purchaseCancelHandler }>
+                { orderSummary }
+            </Modal>
+            { burger }
+        </Aux>
+    );
+
 }
 
 const mapStateToProps = state => {
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
@@ -9,9 +9,8 @@ import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
 import {updateObject, checkValidity} from '../../../shared/utility';
 
-class ContactData extends Component {
-    state = {
-        orderForm: {
+const ContactData = props => {
+    const [orderForm, setOrderForm] = useState({
             name: {
                 elementType: 'input',
                 elementConfig: {
@@ -92,36 +91,36 @@ class ContactData extends Component {
                 validation: {},
                 valid: false,
             },
-        },
-        formIsValid: false,
-    }
+        });
+        const [formIsValid, setFormIsValid] = useState(false);
 
-    orderHandler = (event) => {
+
+    const orderHandler = (event) => {
         event.preventDefault();
 
         const formData = {};
-        for (let formElementIdentifier in this.state.orderForm) {
-            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        for (let formElementIdentifier in orderForm) {
+            formData[formElementIdentifier] = orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ings,
-            price: this.props.price,
+            ingredients: props.ings,
+            price: props.price,
             orderData: formData,
-            userId: this.props.userId
+            userId: props.userId
 
         }
-        this.props.onOrderBurger(order, this.props.token);
+        props.onOrderBurger(order, props.token);
     }
     // 
-    inputChangedHandler = (event, inputIdentifier) => {
+    const inputChangedHandler = (event, inputIdentifier) => {
         
-        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+        const updatedFormElement = updateObject(orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value, orderForm[inputIdentifier].validation),
             touched: true
         });
 
-        const updatedOrderForm = updateObject(this.state.orderForm, {
+        const updatedOrderForm = updateObject(orderForm, {
             [inputIdentifier]: updatedFormElement
         });
 
@@ -129,18 +128,19 @@ class ContactData extends Component {
         for (let inputIdentifier in updatedOrderForm) {
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
         }
-        this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+        setOrderForm(updatedOrderForm);
+        setFormIsValid(formIsValid);
     }
-    render() {
+
         const formElementsArray = [];
-        for (let key in this.state.orderForm) {
+        for (let key in orderForm) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key]
+                config: orderForm[key]
             });
         }
 
-        let form = (<form onSubmit={ this.orderHandler }>
+        let form = (<form onSubmit={ orderHandler }>
             { formElementsArray.map(formElement => (
                 <Input
                     key={ formElement.id }
@@ -151,11 +151,11 @@ class ContactData extends Component {
                     invalid={ !formElement.config.valid }
                     shouldValidate={ formElement.config.validation }
                     touched={ formElement.config.touched }
-                    changed={ (event) => this.inputChangedHandler(event, formElement.id) } />
+                    changed={ (event) => inputChangedHandler(event, formElement.id) } />
             )) }
-            <Button btnType="Success" clicked={ this.orderHandler } disabled={ !this.state.formIsValid }>ORDER</Button>
+            <Button btnType="Success" clicked={ orderHandler } disabled={ !formIsValid }>ORDER</Button>
         </form>);
-        if (this.props.loading) {
+        if (props.loading) {
             form = <Spinner />
         }
         return (
@@ -164,7 +164,6 @@ class ContactData extends Component {
                 { form }
             </div>
         );
-    }
 }
 
 const mapStateToProps = state => {
