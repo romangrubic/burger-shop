7c988d72f0d0a5303fca0cb72a4fb6175f732f8b
Roman Grubic <grubic.roman@gmail.com>
2020-05-07 20:02:48 +0000

    UsingReact.memo() and converting rest of classes to functional components.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types'
 import classes from './BurgerIngredient.module.css'
 
-class BurgerIngredient extends Component {
-    render(){
-        let ingredient = null;
+const BurgerIngredient = props => {
+    let ingredient = null;
 
-    switch (this.props.type) {
+    switch (props.type) {
         case ('bread-bottom'):
             ingredient = <div className={ classes.BreadBottom }></div>;
             break;
@@ -35,8 +34,6 @@ class BurgerIngredient extends Component {
 
     }
     return ingredient;
-    }
-
 };
 
 BurgerIngredient.propTypes = {
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 
-class OrderSummary extends Component {
-
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients)
+const OrderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
             return (
                 <li key={ igKey }>
-                    <span style={ { textTransform: 'capitalize' } }>{ igKey }</span>: { this.props.ingredients[igKey] }
+                    <span style={ { textTransform: 'capitalize' } }>{ igKey }</span>: { props.ingredients[igKey] }
                 </li>);
         });
 
-        return (
-            <Aux>
-                <h3> Your order </h3>
-                <p> Delicious burger with the following ingredients</p>
-                <ul>
-                    { ingredientSummary }
-                </ul>
-                <p><strong>Total price: { this.props.price.toFixed(2) }</strong></p>
-                <p>Continue to Checkout?</p>
-                <Button
-                    btnType="Danger"
-                    clicked={ this.props.purchaseCancelled }>CANCEL</Button>
-                <Button
-                    btnType="Success"
-                    clicked={ this.props.purchaseContinue }>CONTINUE</Button>
-            </Aux>
-        );
-    }
+    return (
+        <Aux>
+            <h3> Your order </h3>
+            <p> Delicious burger with the following ingredients</p>
+            <ul>
+                { ingredientSummary }
+            </ul>
+            <p><strong>Total price: { props.price.toFixed(2) }</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button
+                btnType="Danger"
+                clicked={ props.purchaseCancelled }>CANCEL</Button>
+            <Button
+                btnType="Success"
+                clicked={ props.purchaseContinue }>CONTINUE</Button>
+        </Aux>
+    );
 }
 
 export default OrderSummary;
